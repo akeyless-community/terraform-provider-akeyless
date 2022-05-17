3e8301451bd57e3224518045b0e105d47e067ee9
tuvia-akeyless <tuvia.r@akeyless.io>
2022-05-17 09:24:10 +0300

    try old create func
    to figure out why it deletes other paths

--- akeyless/resource_role.go
+++ akeyless/resource_role.go
@@ -110,19 +110,16 @@ func resourceRole() *schema.Resource {
 	}
 }
 
-func resourceRoleCreate(ctx context.Context, d *schema.ResourceData, m interface{}) (ret diag.Diagnostics) {
+// ---------------------------------------------------------------------------
+
+func resourceRoleCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	provider := m.(providerMeta)
 	client := *provider.client
 	token := *provider.token
 	warn := diag.Diagnostics{}
-	ok := true
 
 	name := d.Get("name").(string)
 	comment := d.Get("comment").(string)
-	auditAccess := d.Get("audit_access").(string)
-	analyticsAccess := d.Get("analytics_access").(string)
-	gwAnalyticsAccess := d.Get("gw_analytics_access").(string)
-	sraReportsAccess := d.Get("sra_reports_access").(string)
 
 	var apiErr akeyless.GenericOpenAPIError
 	body := akeyless.CreateRole{
@@ -130,10 +127,6 @@ func resourceRoleCreate(ctx context.Context, d *schema.ResourceData, m interface
 		Comment: akeyless.PtrString(comment),
 		Token:   &token,
 	}
-	common.GetAkeylessPtr(&body.AuditAccess, auditAccess)
-	common.GetAkeylessPtr(&body.AnalyticsAccess, analyticsAccess)
-	common.GetAkeylessPtr(&body.GwAnalyticsAccess, gwAnalyticsAccess)
-	common.GetAkeylessPtr(&body.SraReportsAccess, sraReportsAccess)
 
 	_, _, err := client.CreateRole(ctx).Body(body).Execute()
 	if err != nil {
@@ -142,36 +135,168 @@ func resourceRoleCreate(ctx context.Context, d *schema.ResourceData, m interface
 		}
 		return diag.Diagnostics{common.ErrorDiagnostics(fmt.Sprintf("can't create Role: %v", err))}
 	}
-	// defer func() {
-	// 	if !ok {
-	// 		deleteRole := akeyless.DeleteRole{
-	// 			Name:  name,
-	// 			Token: &token,
-	// 		}
-
-	// 		_, _, err = client.DeleteRole(ctx).Body(deleteRole).Execute()
-	// 		if err != nil {
-	// 			ret = diag.Diagnostics{common.ErrorDiagnostics(fmt.Sprintf("fatal error: role created with errors and failed to be deleted: %v", err))}
-	// 		}
-	// 	}
-	// }()
 
 	assocAuthMethod := d.Get("assoc_auth_method").([]interface{})
-	err, ok = assocRoleAuthMethod(ctx, name, assocAuthMethod, m)
-	if !ok {
-		return diag.Diagnostics{common.ErrorDiagnostics(err.Error())}
+	if assocAuthMethod != nil {
+		for _, v := range assocAuthMethod {
+			assoc := v.(map[string]interface{})
+			authMethodName := assoc["am_name"].(string)
+			subClaims := assoc["sub_claims"].(map[string]interface{})
+			sc := make(map[string]string, len(subClaims))
+			for k, v := range subClaims {
+				sc[k] = v.(string)
+			}
+
+			asBody := akeyless.AssocRoleAuthMethod{
+				RoleName:  name,
+				AmName:    authMethodName,
+				SubClaims: &sc,
+				Token:     &token,
+			}
+
+			_, _, err = client.AssocRoleAuthMethod(ctx).Body(asBody).Execute()
+			if err != nil {
+				if errors.As(err, &apiErr) {
+					return diag.Diagnostics{common.ErrorDiagnostics(fmt.Sprintf("can't create association: %v", string(apiErr.Body())))}
+				}
+				return diag.Diagnostics{common.ErrorDiagnostics(fmt.Sprintf("can't create association: %v", err))}
+			}
+		}
 	}
 
 	roleRules := d.Get("rules").([]interface{})
-	err, ok = setRoleRule(ctx, name, roleRules, m)
-	if !ok {
-		return diag.Diagnostics{common.ErrorDiagnostics(err.Error())}
+	if roleRules != nil {
+		for _, v := range roleRules {
+			roles := v.(map[string]interface{})
+			capability := roles["capability"].(*schema.Set)
+			path := roles["path"].(string)
+			ruleType := roles["rule_type"].(string)
+
+			if ruleType == "search-rule" || ruleType == "reports-rule" {
+				warn = append(warn, common.WarningDiagnostics(
+					fmt.Sprint("Deprecated: rule types 'search-rule and reports-rule' are deprecated and will be removed, please use 'audit_access' or 'analytics_access' instead")))
+			}
+
+			setRoleRule := akeyless.SetRoleRule{
+				RoleName:   name,
+				Capability: common.ExpandStringList(capability.List()),
+				Path:       path,
+				RuleType:   akeyless.PtrString(ruleType),
+				Token:      &token,
+			}
+			_, _, err := client.SetRoleRule(ctx).Body(setRoleRule).Execute()
+			if err != nil {
+				if errors.As(err, &apiErr) {
+					return diag.Diagnostics{common.ErrorDiagnostics(fmt.Sprintf("can't set rules: %v", string(apiErr.Body())))}
+				}
+				return diag.Diagnostics{common.ErrorDiagnostics(fmt.Sprintf("can't set rules: %v", err))}
+			}
+		}
+	}
+
+	err = updateRole(d, m, ctx)
+	if err != nil {
+		return diag.Diagnostics{common.ErrorDiagnostics(fmt.Sprintf("can't create role: %v", err))}
 	}
 
 	d.SetId(name)
 
 	return warn
 }
+
+func updateRole(d *schema.ResourceData, m interface{}, ctx context.Context) error {
+	provider := m.(providerMeta)
+	client := *provider.client
+	token := *provider.token
+
+	name := d.Get("name").(string)
+	auditAccess := d.Get("audit_access").(string)
+	analyticsAccess := d.Get("analytics_access").(string)
+
+	updateBody := akeyless.UpdateRole{
+		Name:            name,
+		Token:           &token,
+		AuditAccess:     akeyless.PtrString(auditAccess),
+		AnalyticsAccess: akeyless.PtrString(analyticsAccess),
+	}
+
+	var err error
+	var apiErr akeyless.GenericOpenAPIError
+	_, _, err = client.UpdateRole(ctx).Body(updateBody).Execute()
+	if err != nil {
+		if errors.As(err, &apiErr) {
+			return fmt.Errorf("%v", string(apiErr.Body()))
+		}
+		return fmt.Errorf("%v", err)
+	}
+
+	return nil
+}
+
+// ---------------------------------------------------------------------------
+
+// func resourceRoleCreate(ctx context.Context, d *schema.ResourceData, m interface{}) (ret diag.Diagnostics) {
+// 	provider := m.(providerMeta)
+// 	client := *provider.client
+// 	token := *provider.token
+// 	warn := diag.Diagnostics{}
+// 	ok := true
+
+// 	name := d.Get("name").(string)
+// 	comment := d.Get("comment").(string)
+// 	auditAccess := d.Get("audit_access").(string)
+// 	analyticsAccess := d.Get("analytics_access").(string)
+// 	gwAnalyticsAccess := d.Get("gw_analytics_access").(string)
+// 	sraReportsAccess := d.Get("sra_reports_access").(string)
+
+// 	var apiErr akeyless.GenericOpenAPIError
+// 	body := akeyless.CreateRole{
+// 		Name:    name,
+// 		Comment: akeyless.PtrString(comment),
+// 		Token:   &token,
+// 	}
+// 	common.GetAkeylessPtr(&body.AuditAccess, auditAccess)
+// 	common.GetAkeylessPtr(&body.AnalyticsAccess, analyticsAccess)
+// 	common.GetAkeylessPtr(&body.GwAnalyticsAccess, gwAnalyticsAccess)
+// 	common.GetAkeylessPtr(&body.SraReportsAccess, sraReportsAccess)
+
+// 	_, _, err := client.CreateRole(ctx).Body(body).Execute()
+// 	if err != nil {
+// 		if errors.As(err, &apiErr) {
+// 			return diag.Diagnostics{common.ErrorDiagnostics(fmt.Sprintf("can't create Role: %v", string(apiErr.Body())))}
+// 		}
+// 		return diag.Diagnostics{common.ErrorDiagnostics(fmt.Sprintf("can't create Role: %v", err))}
+// 	}
+// 	// defer func() {
+// 	// 	if !ok {
+// 	// 		deleteRole := akeyless.DeleteRole{
+// 	// 			Name:  name,
+// 	// 			Token: &token,
+// 	// 		}
+
+// 	// 		_, _, err = client.DeleteRole(ctx).Body(deleteRole).Execute()
+// 	// 		if err != nil {
+// 	// 			ret = diag.Diagnostics{common.ErrorDiagnostics(fmt.Sprintf("fatal error: role created with errors and failed to be deleted: %v", err))}
+// 	// 		}
+// 	// 	}
+// 	// }()
+
+// 	assocAuthMethod := d.Get("assoc_auth_method").([]interface{})
+// 	err, ok = assocRoleAuthMethod(ctx, name, assocAuthMethod, m)
+// 	if !ok {
+// 		return diag.Diagnostics{common.ErrorDiagnostics(err.Error())}
+// 	}
+
+// 	roleRules := d.Get("rules").([]interface{})
+// 	err, ok = setRoleRule(ctx, name, roleRules, m)
+// 	if !ok {
+// 		return diag.Diagnostics{common.ErrorDiagnostics(err.Error())}
+// 	}
+
+// 	d.SetId(name)
+
+// 	return warn
+// }
 
 func resourceRoleRead(d *schema.ResourceData, m interface{}) error {
 	provider := m.(providerMeta)
@@ -473,7 +598,6 @@ func assocRoleAuthMethod(ctx context.Context, name string, assocAuthMethod []int
 }
 
 func setRoleRule(ctx context.Context, name string, roleRules []interface{}, m interface{}) (error, bool) {
-	var err error
 	var warn error
 
 	provider := m.(providerMeta)
@@ -481,8 +605,6 @@ func setRoleRule(ctx context.Context, name string, roleRules []interface{}, m in
 	token := *provider.token
 
 	for _, v := range roleRules {
-		var apiErr akeyless.GenericOpenAPIError
-
 		roles := v.(map[string]interface{})
 		capability := getCapability(roles["capability"])
 		path := roles["path"].(string)
@@ -491,10 +613,10 @@ func setRoleRule(ctx context.Context, name string, roleRules []interface{}, m in
 			warnMsgToAppend := "Deprecated: rule types 'search-rule and reports-rule' are deprecated and will be removed, please use 'audit_access' or 'analytics_access' instead"
 			warn = fmt.Errorf("%v. %v", warn, warnMsgToAppend)
 		} else if ruleType != "item-rule" && ruleType != "role-rule" && ruleType != "target-rule" && ruleType != "auth-method-rule" {
-			err = fmt.Errorf("wrong rule types: %v", string(apiErr.Body()))
-			return err, false
+			return fmt.Errorf("wrong rule types"), false
 		}
 
+		var apiErr akeyless.GenericOpenAPIError
 		setRoleRule := akeyless.SetRoleRule{
 			RoleName:   name,
 			Capability: capability,
@@ -503,14 +625,12 @@ func setRoleRule(ctx context.Context, name string, roleRules []interface{}, m in
 			Token:      &token,
 		}
 
-		_, _, err = client.SetRoleRule(ctx).Body(setRoleRule).Execute()
+		_, _, err := client.SetRoleRule(ctx).Body(setRoleRule).Execute()
 		if err != nil {
 			if errors.As(err, &apiErr) {
-				err = fmt.Errorf("can't set rules: %v", string(apiErr.Body()))
-				return err, false
+				return fmt.Errorf("can't set rules: %v", string(apiErr.Body())), false
 			}
-			err = fmt.Errorf("can't set rules: %v", err)
-			return err, false
+			return fmt.Errorf("can't set rules: %v", err), false
 		}
 	}
 
