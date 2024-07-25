d640156226f5c12768fe250c52d0e35e78bb2224
eitan-kr <eitan.k@akeyless.io>
2024-07-25 10:25:50 +0300

    add tests

--- akeyless/resource_dynamic_secret_test.go
+++ akeyless/resource_dynamic_secret_test.go
@@ -41,5 +41,60 @@ func TestGithubDynamicSecretResource(t *testing.T) {
 		}
 	`, name, itemPath, GITHUB_INSTALL_ORGANIZATION, GITHUB_TOKEN_REPO, GITHUB_APP_ID, GITHUB_APP_KEY)
 
-	testItemResource(t, itemPath, config, configUpdate, configUpdate2)
+	configUpdate3 := fmt.Sprintf(`
+		resource "akeyless_dynamic_secret_github" "%v" {
+			name            			= "%v"
+			installation_id 			= "%v"
+			installation_organization 	= "%v"
+			token_repositories 			= %v
+			github_app_id 				= %v
+			github_app_private_key 		= "%v"
+		}
+	`, name, itemPath, GITHUB_INSTALL_ID, GITHUB_INSTALL_ORGANIZATION, GITHUB_TOKEN_REPO, GITHUB_APP_ID, GITHUB_APP_KEY)
+
+	configUpdate4 := fmt.Sprintf(`
+		resource "akeyless_dynamic_secret_github" "%v" {
+			name            			= "%v"
+			installation_id 			= "%v"
+			installation_repository 	= "%v"
+			installation_organization 	= "%v"
+			token_repositories 			= %v
+			github_app_id 				= %v
+			github_app_private_key 		= "%v"
+		}
+	`, name, itemPath, GITHUB_INSTALL_ID, GITHUB_INSTALL_REPO, GITHUB_INSTALL_ORGANIZATION, GITHUB_TOKEN_REPO, GITHUB_APP_ID, GITHUB_APP_KEY)
+
+	configUpdate5 := fmt.Sprintf(`
+		resource "akeyless_dynamic_secret_github" "%v" {
+			name            			= "%v"
+			installation_repository 	= "%v"
+			token_repositories 			= %v
+			github_app_id 				= %v
+			github_app_private_key 		= "%v"
+		}
+	`, name, itemPath, GITHUB_INSTALL_REPO, GITHUB_TOKEN_REPO, GITHUB_APP_ID, GITHUB_APP_KEY)
+
+	configUpdate6 := fmt.Sprintf(`
+		resource "akeyless_dynamic_secret_github" "%v" {
+			name            			= "%v"
+			installation_id 			= "%v"
+			installation_repository 	= "%v"
+			installation_organization 	= "%v"
+			token_repositories 			= %v
+			github_app_id 				= %v
+			github_app_private_key 		= "%v"
+		}
+	`, name, itemPath, GITHUB_INSTALL_ID, GITHUB_INSTALL_REPO, GITHUB_INSTALL_ORGANIZATION, GITHUB_TOKEN_REPO, GITHUB_APP_ID, GITHUB_APP_KEY)
+
+	configUpdate7 := fmt.Sprintf(`
+		resource "akeyless_dynamic_secret_github" "%v" {
+			name            			= "%v"
+			installation_id 			= "%v"
+			token_repositories 			= %v
+			github_app_id 				= %v
+			github_app_private_key 		= "%v"
+		}
+	`, name, itemPath, GITHUB_INSTALL_ID, GITHUB_TOKEN_REPO, GITHUB_APP_ID, GITHUB_APP_KEY)
+
+	testItemResource(t, itemPath, config, configUpdate, configUpdate2, configUpdate3, configUpdate4, configUpdate5, configUpdate6, configUpdate7)
 }
