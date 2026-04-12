2f1fef682bc8571156d59ecd59f6c20d31e4037b
tuvia-akeyless <tuvia.r@akeyless.io>
2026-04-12 11:23:36 +0300

    reverts

--- akeyless/tests/gateway/main_test.go
+++ akeyless/tests/gateway/main_test.go
@@ -32,10 +32,6 @@ func TestMain(m *testing.M) {
 		log.Fatalf("gateway not ready: %v", err)
 	}
 
-	if err := testutils.DisableCache(); err != nil {
-		log.Fatalf("failed to disable cache: %v", err)
-	}
-
 	resource.TestMain(m)
 }
 
